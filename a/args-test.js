2deb37aee637b4d48aaa20f919d37dde15383b9d
Kimjammer <john.kim3.1415@gmail.com>
2019-07-24 13:58:15 -0600

    fixed args-test

--- a/args-test.js
+++ a/args-test.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'args-test',
 	description: 'Test if the arguments are working!',
 	execute(message,args) {
-		args.ForEach(element => {
+		args.forEach(element => {
 			message.channel.send(element);
 		});
 		message.channel.send("Done!");
