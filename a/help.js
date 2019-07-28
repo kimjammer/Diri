9fabde360acfa49c17d7c0ba4f5c3ad86b3c3c47
Kimjammer <john.kim3.1415@gmail.com>
2019-07-28 14:12:24 -0600

    fix help

--- a/help.js
+++ a/help.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'See the commands',
 	execute(message,args,client) {
 		let reply = 'Commands:'
-		for (i=0; i <= clinet.listOfCommands.length; i++){
+		for (let i=0; i <= client.listOfCommands.length; i++){
 			reply = reply + `\n ${i+1}. ${client.commands.get(client.listofCommands[i]).name} - ${client.commands.get(client.listofCommands[i]).description} `
 		}
 	}
