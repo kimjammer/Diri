23a69251b1163e907fd4a2c2fd882a98335e5791
Kimjammer <john.kim3.1415@gmail.com>
2019-08-01 13:57:10 -0600

    marspic finished

--- a/marspic.js
+++ a/marspic.js
@@ -4,28 +4,48 @@ module.exports = {
 	execute(message,args,client) {
 		message.channel.send('Looking for a picture...')
 		const randomRoverNum = Math.floor(Math.random()*3);
-		let rover;
+		let rover = "";
 		let queryInfo = {};
-		if (randomRoverNum === 0){
-			rover = "curiosity";
-			queryInfo.sol = Math.floor(Math.random()*5200);
-			queryInfo.cam = 'mast';
-		}else if (randomRoverNum === 1){
-			rover = "opportunity";
-			queryInfo.sol = Math.floor(Math.random()*2300);
-			queryInfo.cam = 'pancam';
-		}else{
-			rover = "spirit";
-			queryInfo.sol = Math.floor(Math.random()*1800);
-			queryInfo.cam = 'pancam'
+		if (args[0]) {
+			if (args[0] == "curiosity") {
+				rover = "curiosity";
+				queryInfo.sol = Math.floor(Math.random()*2463);
+				queryInfo.cam = 'mast';
+			}else if (args[0] == "opportunity") {
+				rover = "opportunity";
+				queryInfo.sol = Math.floor(Math.random()*5111);
+				queryInfo.cam = 'pancam';
+			}else if(args[0] == "spirit") {
+				rover = "spirit";
+				queryInfo.sol = Math.floor(Math.random()*2208);
+				queryInfo.cam = 'pancam';
+			}else{
+				message.channel.send("Please enter a valid rover name. curiosity, spirit, opportunity.")
+				return;
+			}
+		}else { //if no rover argument, then choose randomly
+			if (randomRoverNum === 0){
+				rover = "curiosity";
+				queryInfo.sol = Math.floor(Math.random()*2463);
+				queryInfo.cam = 'mast';
+			}else if (randomRoverNum === 1){
+				rover = "opportunity";
+				queryInfo.sol = Math.floor(Math.random()*5111);
+				queryInfo.cam = 'pancam';
+			}else{
+				rover = "spirit";
+				queryInfo.sol = Math.floor(Math.random()*2208);
+				queryInfo.cam = 'pancam';
+			}
 		}
+		
 		let pictureInfo;
 		client.nasa.MarsPhotos.fetch(rover,{
 			sol: queryInfo.sol,
 			camera: queryInfo.cam
 		})
 		.then(data => {	
-			let picNum = Math.floor(Math.random()*5)
+			let picNum = 1 //Math.floor(Math.random()*5)
 			const attachment = new client.attachment(data.photos[picNum].img_src);
 			message.channel.send(attachment);
 			message.channel.send(`This is from the ${data.photos[picNum].rover.name} rover on sol ${data.photos[picNum].sol}. Taken on the ${data.photos[picNum].camera.full_name}.`)
