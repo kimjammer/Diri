c82b6abf01b4c29d49e216bf9f14e4405f1a38c9
kimjammer <41841812+kimjammer@users.noreply.github.com>
2021-01-10 10:35:55 -0700

    Added ?xkcd random feature

--- src/a/xkcd.js
+++ src/a/xkcd.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'xkcd',
     description: 'Returns today\'s comic or a specific one by id.',
-    usage: `?xkcd [optional: comic number]`,
+    usage: `?xkcd [optional: comic number/random to get a random comic]`,
     category: "fun",
     guildOnly: false,
     execute(message,args,client) {
@@ -19,7 +19,7 @@ module.exports = {
             message.channel.send(embed);
         };
 
-        const getxkcd = (xkcdUrl, isNumCheck) => {
+        const getxkcd = (xkcdUrl, isNumCheck, isGetLatestNum = false) => { //Yeah, yeah, having three args to change what the function does is dumb. I don't have the effort to do it properly right now.
             const xhr = new client.XMLHttpRequest();
 
             xhr.responseType = 'json';
@@ -28,7 +28,9 @@ module.exports = {
                 if (xhr.readyState === client.XMLHttpRequest.DONE) {
                 	if (isNumCheck) {
                         determineXkcdNumValidity (xhr.response.num);
-                	}else {
+                	}else if (isGetLatestNum){
+                	    sendRandomComic(xhr.response.num);
+                    }else {
                 		sendResult(xhr.response);
                 	}
                 }
@@ -46,11 +48,28 @@ module.exports = {
             }
         }
 
+        function sendRandomComic (maxXkcdNum) {
+            //Choose a random number from 1 to the latest xkcd number
+            let randomXkcdNum = Math.floor(Math.random() * maxXkcdNum) + 1;
+            //Create the xkcd url with the random number
+            xkcdUrl = `http://xkcd.com/${randomXkcdNum}/info.0.json`;
+            //Get and send the results
+            getxkcd(xkcdUrl,false);
+            //Tell the user what their random number was
+            message.channel.send(`Your random comic was number ${randomXkcdNum}`);
+        }
+
         let xkcdUrl;
 
+        //If there are arguments, ie a comic number is specified or it says random comic.
         if (args[0]) {
-            //call getxkcd, but with the isNumCheck on. This will tell it to check the num, and call itself again if the number is valid.
-            getxkcd("http://xkcd.com/info.0.json", true);
+            if (args[0] == "random") {
+                //This gets the latest comic's number and passes it to the sendRandomComic function
+                getxkcd("http://xkcd.com/info.0.json",false,true);
+            }else{
+                //call getxkcd, but with the isNumCheck on. This will tell it to check the num, and call itself again if the number is valid.
+                getxkcd("http://xkcd.com/info.0.json", true);
+            }
         }else {
             xkcdUrl = "http://xkcd.com/info.0.json"
             getxkcd(xkcdUrl, false);
